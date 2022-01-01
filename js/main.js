5f2752d5ebd61d04ee394cfa53472415933f0c7b
david-c-oliveros <david3oliveros@gmail.com>
2022-01-01 03:35:38 +0000

    Implement sprite animation with multiple sprite frames

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 /*****************************/
 /*         Constants         */
 /*****************************/
-//const SCREEN_WIDTH = document.querySelector('canvas').offsetWidth;
-//const SCREEN_HEIGHT = document.querySelector('canvas').offsetHeight;
+
 const SCREEN_WIDTH = document.querySelector('canvas').width;
 const SCREEN_HEIGHT = document.querySelector('canvas').height;
 
 // Sprite constants
+const SPRITE_SHEETS = ['images/galaga_general_spritesheet.png', 'galaga_screens_and_text_spritesheet.png'];
 const SPRITE_WIDTH = 16;
 const SPRITE_HEIGHT = 16;
 const BORDER_WIDTH = 1;
 const SPACING_WIDTH = 2;
 const SPRITE_SCALE = 3;
 
+const GAME_TICK = 20;
 const MOVE_SPEED = 10;
 
 console.log('Screen width: ',   SCREEN_WIDTH);
@@ -26,6 +27,11 @@ console.log('Screen height: ',   SCREEN_HEIGHT);
 /**********************************/
  
 let keys = [];
+let t1 = new Date().getTime();
+let t2 = new Date().getTime();
+let elapsedTime = 0;
+let executeTime = 0;
+let tickCount = 0;
 
 
 
@@ -47,16 +53,13 @@ class Game
 
         this.players = [];
         this.enemies = [];
+        this.spriteSheets = [];
+        this.projectiles = [];
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas.getContext('2d');
         this.canvasStyle = window.getComputedStyle(canvas);
-        console.log(this.openPos.x, this.openPos.y);
     }
 
-
-    update()
-    {
-    }
 
     handleKeyEvent()
     {
@@ -96,74 +99,30 @@ class Game
             this.players[0].xVel = 0;
         }
 
-//        /*******************************/
-//        /*       W Key - Move Up       */
-//        /*******************************/
-//        if (keys[87])
-//        {
-//            if (this.players[0].yPos < 0)
-//            {
-//                this.players[0].yPos = 1;
-//            } else
-//            {
-//                this.players[0].yVel -= MOVE_SPEED;
-//                this.players[0].yPos += this.players[0].yVel;
-//            }
-//        } else
-//        {
-//            this.players[0].yVel = 0;
-//        }
-//
-//        /*********************************/
-//        /*       S Key - Move Down       */
-//        /*********************************/
-//        if (keys[83])
-//        {
-//            if (this.players[0].yPos > SCREEN_HEIGHT)
-//            {
-//                this.players[0].yPos = SCREEN_HEIGHT - 1;
-//            } else
-//            {
-//                this.players[0].yVel += MOVE_SPEED;
-//                this.players[0].yPos += this.players[0].yVel;
-//            }
-//        } else
-//        {
-//            this.players[0].yVel = 0;
-//        }
-
-
-//        /*****************************************/
-//        /*       E Key - Add Enemy (Debug)       */
-//        /*****************************************/
-//        if (keys[69])
-//        {
-//            if (this.openPos.x < SCREEN_WIDTH && this.openPos.y < SCREEN_HEIGHT)
-//            {
-//                this.openPos.x += SPRITE_WIDTH;
-//                this.addObject(new Enemy(this.canvas, this.openPos.x, this.openPos.y, '', 1, 7), 'enemy');
-//            } else if (this.openPos.x >= SCREEN_WIDTH && this.openPos.y < SCREEN_HEIGHT)
-//            {
-//                this.openPos.x = 20;
-//                this.openPos.y += SPRITE_HEIGHT;
-//                this.addObject(new Enemy(this.canvas, this.openPos.x, this.openPos.y, '', 1, 7), 'enemy');
-//            } else
-//            {
-//                this.openPos.x = 20;
-//                this.openPos.y = 50;
-//            }
-//        }
-
-        // P Key - Debug
-        if (keys[80])
+        /*******************************************/
+        /*       Space Key - FIre Projectile       */
+        /*******************************************/
+        if (keys[32] && !this.players[0].coolDown)
         {
-            this.debug();
+            this.addObject(new Projectile(this.canvas, this.players[0].xPos, this.players[0].yPos - (SPRITE_HEIGHT * SPRITE_SCALE), 1, 1), 'projectile');
+            this.players[0].coolDown = true;
+        }
+    }
+
+
+    update()
+    {
+        if (tickCount % 5 === 0)
+        {
+            for (let i = 0; i < this.enemies.length; i++)
+            {
+                this.enemies[i].update();
+            }
         }
 
-        // Space Key - Fire projectile
-        if (keys[32])
+        for (let i = 0; i < game.projectiles.length; i++)
         {
-            this.addObject(new Projectile(this.players[0].xPos, this.players[0].yPos), 'projectile');
+            game.projectiles[i].yPos -= 10;
         }
     }
 
@@ -180,14 +139,9 @@ class Game
                 break;
             case('projectile'):
                 console.log('Fired projectile');
+                this.projectiles.push(object);
                 break;
         }
-    }
-
-
-    addDomElement(element)
-    {
-        this.canvas.appendChild(element);
     }
 
 
@@ -196,12 +150,32 @@ class Game
         this.context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
         for (let i = 0; i < this.players.length; i++)
         {
-            this.players[i].sprite.draw(this.canvas.getContext('2d'), this.players[i].pos, this.players[i].xPos, this.players[i].yPos);
+            this.players[i].spriteFrames[this.players[i].currentFrame].draw(this.canvas.getContext('2d'), this.players[i].xPos, this.players[i].yPos);
         }
 
         for (let i = 0; i < this.enemies.length; i++)
         {
-            this.enemies[i].sprite.draw(this.canvas.getContext('2d'), this.enemies[i].pos, this.enemies[i].xPos, this.enemies[i].yPos);
+            this.enemies[i].spriteFrames[this.enemies[i].currentFrame].draw(this.canvas.getContext('2d'), this.enemies[i].xPos, this.enemies[i].yPos);
+        }
+
+        for (let i = 0; i < this.projectiles.length; i++)
+        {
+            this.projectiles[i].spriteFrames[0].draw(this.canvas.getContext('2d'), this.projectiles[i].xPos, this.projectiles[i].yPos);
+        }
+    }
+
+
+    checkCollision()
+    {
+        for (let i = 0; i < this.enemies.length; i++)
+        {
+            for (let j = 0; j < this.projectiles.length; j++)
+            {
+                if (Math.abs(this.projectiles[j].xPos - this.enemies[i].xPos) < 10 && Math.abs(this.projectiles[j].yPos - this.enemies[i].yPos < 10))
+                {
+                    this.enemies[i].state = 'exploding';
+                }
+            }
         }
     }
 
@@ -222,80 +196,119 @@ class Game
 
 class Obj
 {
-    constructor(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols, imgPath = 'images/player.png')
+    constructor(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols)
     {
         this.canvas = canvas;
         this.xPos = xPos;
         this.yPos = yPos;
-        this.imgPath = imgPath;
         this.nSpriteSheetRows = nSpriteSheetRows;
         this.nSpriteSheetCols = nSpriteSheetCols;
         this.spriteFrames = [];
+        this.currentFrame = 0;
     }
 
-    genSprites(spriteStartRow, spriteStartCol)
+    genSprites(spriteFile, spriteStartRow, spriteStartCol)
     {
+        let sheetPos;
         for (let i = 0; i < this.nSpriteSheetRows; i++)
         {
             for (let j = 0; j < this.nSpriteSheetCols; j++)
             {
                 // Add sprite frames to object's sprite array
+                sheetPos = spritePosToImagePos(spriteStartRow + i, spriteStartCol + j);
+                this.spriteFrames[i + j] = new Sprite(this.canvas, spriteFile, SPRITE_WIDTH, SPRITE_HEIGHT, sheetPos, BORDER_WIDTH, SPACING_WIDTH);
             }
         }
-        this.pos = spritePosToImagePos(spriteStartRow, spriteStartCol);
-        this.sprite = new Sprite(this.canvas, img, this.pos.x, this.pos.y, this.nSpriteSheetRows, this.nSpriteSheetCols, BORDER_WIDTH, SPACING_WIDTH)
-        let self = this;
-        img.onload = function() {
-//            self.canvas.width = this.naturalWidth;
-//            self.canvas.height = this.naturalHeight;
-            self.sprite.draw(context, self.pos, self.xPos, self.yPos);
-        };
     }
 }
+
 
 
 class Player extends Obj
 {
-    constructor(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols)
+    constructor(canvas, xPos, yPos, spriteSheet, nSpriteSheetRows, nSpriteSheetCols, nExplodeSpriteSheetRows = 1, nExplodeSpriteSheetCols = 5)
     {
-        super(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols);
+        super(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols);
 
-        this.genSprites(0, 6);
+        this.genSprites(spriteSheet, 0, 6);
         this.xVel = 0;
         this.yVel = 0;
-
+        this.coolDown = false;
+        this.currentSpriteFrames = 0;
     }
 }
+
 
 
 class Enemy extends Obj
 {
-    constructor(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols, enemyNum)
+    constructor(canvas, xPos, yPos, spriteSheet, nSpriteSheetRows, nSpriteSheetCols, enemyNum)
     {
-        super(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols);
+        super(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols);
 
         this.enemyNum = enemyNum + 2;
-        this.genSprites(this.enemyNum, 0);
+        this.genSprites(spriteSheet, this.enemyNum, 0);
+        this.currentState = 'resting';
         this.xVel = 0;
         this.yVel = 0;
+    }
+
+    update()
+    {
+        switch(this.currentState)
+        {
+            case('resting'):
+                if (this.currentFrame != this.nSpriteSheetCols - 1)
+                {
+                    this.currentFrame = this.nSpriteSheetCols - 1;
+                } else {
+                    this.currentFrame = this.nSpriteSheetCols - 2;
+                }
+                break;
+            case('attacking'):
+                break;
+            case('exploding'):
+                break;
+        }
+    }
+
+    destroy()
+    {
     }
 }
 
 
+
 class Projectile extends Obj
 {
-    constructor(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols)
+    constructor(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols)
     {
-        super(canvas, xPos, yPos, imgPath, nSpriteSheetRows, nSpriteSheetCols);
+        super(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols);
+        this.genSprites(6.5, 17);
         this.xPos = xPos;
         this.yPos = yPos;
+        this.yVel = 10;
+    }
+
+    update()
+    {
+        this.yPos += this.yVel;
+    }
+}
+
+
+
+class Explosion extends Obj
+{
+    constructor(canvas, xPos, yPos, nSpriteSheetRows, nSpriteSheetCols)
+    {
     }
 }
 
 
 class Sprite
 {
-    constructor(canvas, spriteSheet, spriteWidth, spriteHeight, startRow, startCol, nRows, nCols, borderWidth, spacingWidth)
+    constructor(canvas, spriteSheet, spriteWidth, spriteHeight, sheetPos, borderWidth, spacingWidth)
     {
         this.canvas = canvas;
         this.spriteSheet = spriteSheet;
@@ -303,20 +316,17 @@ class Sprite
         this.spriteHeight = spriteHeight;
         this.borderWidth = borderWidth;
         this.spacingWidth = spacingWidth;
-        this.startRow = startRow;
-        this.startCol = startCol;
-        this.nRows = nRows;
-        this.nCols = nCols;
+        this.sheetPos = sheetPos;
         this.spriteImage = new Image();
         this.spriteImage.src = this.spriteSheet;
     }
 
-    draw(context, sheetPos, xPos, yPos)
+    draw(context, xPos, yPos)
     {
         context.drawImage(
             this.spriteSheet,
-            sheetPos.x,
-            sheetPos.y,
+            this.sheetPos.x,
+            this.sheetPos.y,
             SPRITE_WIDTH,
             SPRITE_HEIGHT,
             xPos, yPos,
@@ -334,11 +344,9 @@ class Sprite
 
 function init()
 {
-    // First we'll add our player spaceship
+    // First we'll add our elayer spaceship
     const playerPos = (SCREEN_WIDTH / 2) - (SPRITE_WIDTH / 2);
-    console.log('Player Position:', playerPos);
-    console.log('Screen Width:', SCREEN_WIDTH);
-    game.addObject(new Player(game.canvas, playerPos, SCREEN_HEIGHT - (SCREEN_HEIGHT / 7), 1, 7, 'images/player_ship.png'), 'player');
+    game.addObject(new Player(game.canvas, playerPos, SCREEN_HEIGHT - (SCREEN_HEIGHT / 7), game.spriteSheets[0], 1, 7), 'player');
 
     // Lets add an assortment of differnet enemies to test the API
     const nEnemies = 10;
@@ -347,10 +355,11 @@ function init()
     let eNum;
     for (let i = 0; i < nEnemies; i++)
     {
-        eNum = Math.floor(Math.random() * 10);
-        console.log('eNum:', eNum);
-        game.addObject(new Enemy(game.canvas, (i * SPRITE_WIDTH * spacing * SPRITE_SCALE) + startPos, 50, '', 1, 7, eNum), 'enemy');
+        eNum = Math.floor(Math.random() * 4);
+        game.addObject(new Enemy(game.canvas, (i * SPRITE_WIDTH * spacing * SPRITE_SCALE) + startPos, 50, game.spriteSheets[0], 1, 8, eNum), 'enemy');
     }
+
+    game.update();
 
 
     // Event Listeners
@@ -359,14 +368,25 @@ function init()
     });
 
     document.addEventListener('keyup', function(e) {
-        console.log(e.keyCode);
         delete keys[e.keyCode];
     });
-
-//    setInterval(gameLoop, 2000);
 }
 
-function spritePosToImagePos(row, col)
+function spritePosToImagePosSmall(row, col)
+{
+    return {
+        x: (
+            BORDER_WIDTH +
+                col * (SPACING_WIDTH + SPRITE_WIDTH)
+        ),
+        y: (
+            BORDER_WIDTH +
+            row * (SPACING_WIDTH + SPRITE_HEIGHT)
+        )
+    }
+}
+
+function spritePosToImagePosMedium(row, col)
 {
     return {
         x: (
@@ -382,9 +402,25 @@ function spritePosToImagePos(row, col)
 
 function gameLoop()
 {
+    t2 = new Date().getTime();
+    elapsedTime = t2 - t1;
+    t1 = t2;
+    executeTime += elapsedTime;
+
+    if (tickCount > 9000000000)
+    {
+        tickCount = 0;
+    }
+
+    if (executeTime >= GAME_TICK)
+    {
+        tickCount++;
+        game.update();
+        executeTime = 0;
+    }
+
     game.handleKeyEvent();
 
-    game.update();
     game.render();
     window.requestAnimationFrame(gameLoop);
 }
@@ -397,22 +433,15 @@ function gameLoop()
 /******************************/
 
 const game = new Game();
+game.spriteSheets = [];
 
-//game.addObject(500, 500, 'images/enemy.png');
-const img = new Image();
-img.src = 'images/galaga_general_spritesheet.png';
+for (let i = 0; i < SPRITE_SHEETS.length; i++)
+{
+    game.spriteSheets[i] = new Image();
+    game.spriteSheets[i].src = SPRITE_SHEETS[i];
+}
+
 let context = game.canvas.getContext('2d');
-//let pos = spritePosToImagePos(4, 3);
-//let sprite = new Sprite(game.canvas, img, pos.x, pos.y, 1, 7, BORDER_WIDTH, SPACING_WIDTH)
-//img.onload = sprite.draw(context, pos, 100, 100);
-//img.onload = sprite.draw;
-/*
-img.onload = function() {
-    game.canvas.width = this.naturalWidth;
-    game.canvas.height = this.naturalHeight;
-    sprite.draw(context, pos, 100, 100);
-};
-*/
 
 init();
 window.requestAnimationFrame(gameLoop);
